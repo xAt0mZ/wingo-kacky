fae85a774abe34e5032ecef408206377ecc14a19
LP B <xAt0mZ@users.noreply.github.com>
2021-08-18 03:10:38 +0200

    feat: generic URL ranges build

--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,10 @@
-import axios from "axios";
+import axios from 'axios';
+
+const sheets = ['WINGO', 'JR'];
 
 const baseURL = 'https://sheets.googleapis.com/v4/spreadsheets/1Yqkhv4ayhwFkV39W0W7GLt1FnpHLUUXtpgGvRklnDnI/values:batchGet';
 const key = `key=${process.env.REACT_APP_GOOGLE_API_KEY}`;
-const wingoRanges = 'ranges=WINGO!A2:F33&ranges=WINGO!G2:L33&ranges=WINGO!M2:R12';
-const jrRanges = 'ranges=JR!A2:F33&ranges=JR!G2:L33&ranges=JR!M2:R12';
-const ranges = `${wingoRanges}&${jrRanges}`;
+const ranges = sheets.map((s) => `ranges=${s}!A2:F33&ranges=${s}!G2:L33&ranges=${s}!M2:R12`).join('&');
 const options = 'valueRenderOption=FORMULA&dateTimeRenderOption=FORMATTED_STRING'
 const url = `${baseURL}?${key}&${ranges}&${options}`
 

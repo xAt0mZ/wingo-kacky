d87f6a683de2ef83a0ec4805129c5c5c47013b20
LP B <xAt0mZ@users.noreply.github.com>
2022-01-12 03:17:34 +0100

    refactor: remove comment

--- src/timers/components/ServersCounters.tsx
+++ src/timers/components/ServersCounters.tsx
@@ -6,7 +6,6 @@ export function ServerCounters() {
       <ServerInfo id={1}/>
       <ServerInfo id={2}/>
       <ServerInfo id={3}/>
-      {/* <img src="https://www.dingens.me/kack_thumbnails/245.jpg" width="320" className="map-thumbnail" alt="thumb" /> */}
     </>
   );
 }

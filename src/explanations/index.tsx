2a0ad0d27d586d34f4b8cd0caf73db1a301825df
LP B <xAt0mZ@users.noreply.github.com>
2022-08-19 20:24:07 +0200

    feat: add 2nd video of explanations

--- src/explanations/index.tsx
+++ src/explanations/index.tsx
@@ -1,4 +1,4 @@
-import { Row } from 'react-bootstrap';
+import { Col, Row } from 'react-bootstrap';
 
 import { VStack } from '../components/VStack';
 
@@ -6,14 +6,26 @@ export function Explanations() {
   return (
     <VStack>
       <Row className='flex-fill h-100'>
+        <Col>
         < iframe
           width="100%"
-          height="100%"
+          height="50%"
           src="https://www.youtube-nocookie.com/embed/WyY7Yihd_1o"
           title="Le Kacky - Explications"
           frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           allowFullScreen />
+        </Col>
+        <Col>
+        < iframe
+          width="100%"
+          height="50%"
+          src="https://www.youtube-nocookie.com/embed/caX6BicmMos"
+          title="Le Kacky - Explications"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen />
+        </Col>
       </Row>
     </VStack>
   );

bb520fdf9877458e31392d7edecec836b0108421
LP B <xAt0mZ@users.noreply.github.com>
2023-07-06 12:05:00 +0200

    refactor(app): migrate to vite + prepare v2

--- src/index.tsx
+++ src/index.tsx
@@ -1,39 +1,8 @@
-import { StrictMode } from 'react';
-import { render } from 'react-dom';
-import { Chart as ChartJS, Tooltip, Legend, CategoryScale, LinearScale, BarElement, BarController, PointElement, LineElement, LineController } from 'chart.js';
+import { createRoot } from 'react-dom/client'
+import 'tailwindcss/tailwind.css'
+import App from 'components/App'
 
-import './index.css'
-import { App } from './App';
+const container = document.getElementById('root') as HTMLDivElement
+const root = createRoot(container)
 
-ChartJS.register(
-  CategoryScale,
-  LinearScale,
-  BarElement,
-  PointElement,
-  LineElement,
-  BarController,
-  LineController,
-  Tooltip,
-  Legend
-);
-
-ChartJS.defaults.color = "#F8F9FA";
-ChartJS.defaults.font = {
-  ...ChartJS.defaults.font,
-  family: 'Proxima Nova',
-  weight: 'lighter',
-  style: 'normal'
-};
-
-render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-  ,
-  document.getElementById('root')
-);
-
-// // If you want to start measuring performance in your app, pass a function
-// // to log results (for example: reportWebVitals(console.log))
-// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
+root.render(<App />)

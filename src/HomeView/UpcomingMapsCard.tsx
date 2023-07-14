3e83eb466beff6ac718ca921ee645cd0bad582cb
LP B <xAt0mZ@users.noreply.github.com>
2023-07-14 22:39:41 +0200

    feat(home): upcoming maps

--- src/HomeView/UpcomingMapsCard.tsx
+++ src/HomeView/UpcomingMapsCard.tsx
@@ -1,5 +1,42 @@
+import { intlFormatDistance } from 'date-fns';
+import { upperFirst } from 'lodash';
+
 export function UpcomingMapsCard() {
-  return null;
+  return (
+    <div className="flex flex-col gap-5 rounded-2xl bg-gray-light p-4">
+      <span className="text-lg font-bold text-purple-blue">À venir</span>
+      <div className="flex flex-col gap-2">
+        <Item mapNumber={200} server={3} time={new Date()} />
+        <Item mapNumber={200} server={3} time={new Date()} />
+        <Item mapNumber={200} server={3} time={new Date()} />
+        <Item mapNumber={200} server={10} time={new Date()} />
+      </div>
+    </div>
+  );
+}
+
+type ItemProps = {
+  mapNumber: number;
+  time: Date;
+  server: number;
+};
+
+function Item({ mapNumber, time, server }: ItemProps) {
+  const distance = intlFormatDistance(time, new Date(), { locale: 'fr-FR' });
+
+  return (
+    <>
+      <div className="inline-flex items-center justify-between">
+        <span className="rounded-2xl border border-gray-medium bg-white px-4 py-2 text-base font-medium text-purple-blue">{mapNumber}</span>
+        <div className="inline-flex items-center gap-2">
+          <div className="text-right text-base font-medium text-purple">{upperFirst(distance)}</div>
+          <div className="h-4 w-0 border border-gray-medium"></div>
+          <div className="text-left text-base font-medium text-purple-blue">Serveur {server}</div>
+        </div>
+      </div>
+      <div className="border border-gray-medium last:hidden" />
+    </>
+  );
 }
 
 function upcomingMapsCard() {

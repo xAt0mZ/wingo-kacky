c2a69a628c1897bbe3f04f1f3aec12e7aa06e599
LP B <xAt0mZ@users.noreply.github.com>
2023-07-17 18:56:44 +0200

    dev(app): expose local yarn start

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tsconfigPaths from 'vite-tsconfig-paths'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config https://vitest.dev/config
 export default defineConfig({
@@ -9,6 +9,6 @@ export default defineConfig({
     globals: true,
     environment: 'happy-dom',
     setupFiles: '.vitest/setup',
-    include: ['**/test.{ts,tsx}']
-  }
-})
+    include: ['**/test.{ts,tsx}'],
+  },
+});

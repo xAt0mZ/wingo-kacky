b2d4c67834746a3edaed4ca3c14abe26698d1269
LP B <xAt0mZ@users.noreply.github.com>
2023-07-27 15:06:47 +0200

    feat(home): responsive rank card at 2xl+

--- src/HomeView/RankCard/RankCard.tsx
+++ src/HomeView/RankCard/RankCard.tsx
@@ -8,18 +8,18 @@ export function RankCard() {
   const rankName = 'Plastique';
 
   return (
-    <div className="relative">
+    <div className="relative h-full">
       <img width={176} height={108} className="absolute" src={carImage} />
-      <div className="shrink-0">
+      <div className="h-full shrink-0">
         <div className="h-6" />
-        <div className="flex shrink-0 flex-col items-stretch rounded-2xl bg-gray-light p-4">
+        <div className="flex h-[calc(100%-1.5rem)] shrink-0 flex-col items-stretch rounded-2xl bg-gray-light p-4">
           <div className="h-[68px]" />
-          <div className="flex flex-row items-stretch justify-between">
-            <div className="flex flex-col justify-evenly">
+          <div className="flex h-full flex-row items-stretch justify-between">
+            <div className="flex grow flex-col justify-end">
               <span className="text-sm font-normal text-gray-dark">Prochain rang</span>
               <span className="text-base font-bold text-purple-blue">{rankName}</span>
             </div>
-            <div className="grow pl-2">
+            <div className="flex grow items-end pl-2">
               <ProgressBar min={min} max={max} value={value} />
             </div>
           </div>

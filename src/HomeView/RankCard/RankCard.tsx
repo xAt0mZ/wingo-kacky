388e98d58098413cae48155a5f23bc6145ab40ee
LP B <xAt0mZ@users.noreply.github.com>
2023-08-06 20:07:23 +0200

    feat(home): ranks for rank card

--- src/HomeView/RankCard/RankCard.tsx
+++ src/HomeView/RankCard/RankCard.tsx
@@ -1,15 +1,39 @@
 import carImage from './car.png';
 import { ProgressBar } from './ProgressBar';
 
+type Rank = {
+  name: string;
+  min: number;
+  max: number;
+  image: string;
+};
+
+function newRank(name: string, min: number, max: number, image: string): Rank {
+  return {
+    name,
+    min,
+    max,
+    image,
+  };
+}
+
+const ranks: Rank[] = [
+  newRank('Plastique', 0, 10, carImage),
+  newRank('Bronze', 10, 25, carImage),
+  newRank('Argent', 25, 50, carImage),
+  newRank('Or', 50, 65, carImage),
+  newRank('Légende', 65, 75, carImage),
+];
+
 export function RankCard() {
-  const min = 0;
-  const value = 5;
-  const max = 10;
-  const rankName = 'Plastique';
+  const value = 200;
+  const { name, image, min, max } =
+    ranks.find((r) => r.min <= value && value < r.max) ||
+    newRank('Inconnu', 0, 0, carImage);
 
   return (
     <div className="relative h-full">
-      <img width={176} height={108} className="absolute" src={carImage} />
+      <img width={176} height={108} className="absolute" src={image} />
       <div className="h-full shrink-0">
         <div className="h-6" />
         <div className="flex h-[calc(100%-1.5rem)] shrink-0 flex-col items-stretch rounded-2xl bg-theme-6 p-4">
@@ -19,9 +43,7 @@ export function RankCard() {
               <span className="text-sm font-normal text-theme-3">
                 Prochain rang
               </span>
-              <span className="text-base font-bold text-theme-2">
-                {rankName}
-              </span>
+              <span className="text-base font-bold text-theme-2">{name}</span>
             </div>
             <div className="flex grow items-end pl-2">
               <ProgressBar min={min} max={max} value={value} />

0f4a55b3b7177e77658ea33c266b954b7504c81e
LP B <xAt0mZ@users.noreply.github.com>
2023-08-11 21:07:45 +0200

    refactor(maps): migrate filter to Modal component

--- src/MapsView/Filters.tsx
+++ src/MapsView/Filters.tsx
@@ -1,6 +1,6 @@
 import clsx from 'clsx';
 import { XMarkIcon, Bars2Icon, CheckIcon } from '@heroicons/react/24/outline';
-import { Overlay } from 'components/Overlay';
+import { Modal, ModalProvider, useModalContext } from 'components/Modal';
 
 export function Filters() {
   return (
@@ -17,45 +17,56 @@ export function Filters() {
 
 function MicroFilters() {
   return (
-    <Overlay>
-      <Overlay.Collapsed>
-        {({ show }) => (
-          <button
-            onClick={show}
-            className="flex w-full flex-row items-center justify-center gap-2 rounded-lg bg-theme-6 px-6 py-3.5 text-theme-2"
-          >
-            <Bars2Icon className="h-4 w-4" />
-            <span className="text-base font-medium">Filtrer</span>
-          </button>
-        )}
-      </Overlay.Collapsed>
-      <Overlay.Expanded className="flex !h-full w-full flex-col justify-between bg-theme-6 p-4">
-        {({ hide }) => (
-          <>
-            <div className="flex flex-col items-start gap-8 text-theme-2">
-              <button onClick={hide} className="self-end">
-                <XMarkIcon className="h-6 w-6" />
-              </button>
-              <div className="flex flex-col items-start gap-6 self-stretch">
-                <span className="text-4xl font-bold text-theme-2">Filtrer</span>
-                <Items />
-              </div>
-            </div>
-            <button
-              onClick={hide}
-              className={clsx(
-                'flex flex-row items-center justify-center gap-2 self-stretch rounded-lg px-6 py-3.5',
-                'bg-theme-4 text-white-neutral',
-                'dark:border dark:border-theme-4 dark:bg-theme-6'
-              )}
-            >
-              <CheckIcon className="h-4 w-4" />
-              <span className="text-base font-medium">Valider les filtres</span>
+    <ModalProvider>
+      <Content />
+    </ModalProvider>
+  );
+}
+
+function Content() {
+  const { isOpen, setIsOpen, show, hide } = useModalContext();
+  return (
+    <>
+      <button
+        onClick={show}
+        className="flex w-full flex-row items-center justify-center gap-2 rounded-lg bg-theme-6 px-6 py-3.5 text-theme-2"
+      >
+        <Bars2Icon className="h-4 w-4" />
+        <span className="text-base font-medium">Filtrer</span>
+      </button>
+
+      <Modal
+        className="flex h-full w-full flex-col justify-between bg-theme-6 p-4"
+        show={isOpen}
+        setShow={setIsOpen}
+        transition="ease-in-out duration-500"
+        from="opacity-0"
+        to="opacity-100"
+      >
+        <>
+          <div className="flex flex-col items-start gap-8 text-theme-2">
+            <button onClick={hide} className="self-end">
+              <XMarkIcon className="h-6 w-6" />
             </button>
-          </>
-        )}
-      </Overlay.Expanded>
-    </Overlay>
+            <div className="flex flex-col items-start gap-6 self-stretch">
+              <span className="text-4xl font-bold text-theme-2">Filtrer</span>
+              <Items />
+            </div>
+          </div>
+          <button
+            onClick={hide}
+            className={clsx(
+              'flex flex-row items-center justify-center gap-2 self-stretch rounded-lg px-6 py-3.5',
+              'bg-theme-4 text-white-neutral',
+              'dark:border dark:border-theme-4 dark:bg-theme-6'
+            )}
+          >
+            <CheckIcon className="h-4 w-4" />
+            <span className="text-base font-medium">Valider les filtres</span>
+          </button>
+        </>
+      </Modal>
+    </>
   );
 }
 

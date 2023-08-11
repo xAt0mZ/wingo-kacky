14f8ffbc2313c505153dc42f42cbdb4ef717e306
LP B <xAt0mZ@users.noreply.github.com>
2023-08-11 19:40:01 +0200

    refactor(app): relocate theme at DOM root

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,10 @@
 import { Outlet } from 'react-router-dom';
 
 import { Sidebar } from 'components/Sidebar';
-import { useTheme } from 'hooks/useTheme';
 
 export function App() {
-  const { theme, colorblind } = useTheme();
-
   return (
-    <div
-      className="flex h-full w-full flex-col gap-4 bg-theme-7 pb-24 sm:min-h-screen sm:flex-row sm:px-8 sm:py-6"
-      data-theme={theme}
-      data-colorblind={colorblind}
-    >
+    <div className="flex h-full w-full flex-col gap-4 bg-theme-7 pb-24 sm:min-h-screen sm:flex-row sm:px-8 sm:py-6">
       <Sidebar />
       <div className="flex grow flex-col gap-6 px-4 sm:pr-0">
         <Outlet />

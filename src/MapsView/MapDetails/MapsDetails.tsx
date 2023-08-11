952592ca82e89c008d4d3347d6558a81d1c3dd7c
LP B <xAt0mZ@users.noreply.github.com>
2023-08-11 19:45:43 +0200

    refactor(app): prettier cleanup

--- src/MapsView/MapDetails/MapsDetails.tsx
+++ src/MapsView/MapDetails/MapsDetails.tsx
@@ -1,9 +1,9 @@
-import { TMMap } from 'api/types';
+// import { TMMap } from 'api/types';
 
-type Props = {
-  map: TMMap;
-};
+// type Props = {
+//   map: TMMap;
+// };
 
-export function MapDetails({ map }: Props) {
-  return <div className="bg-red-500 h-full w-full" />;
+export function MapDetails() {
+  return <div className="h-full w-full bg-red-500" />;
 }

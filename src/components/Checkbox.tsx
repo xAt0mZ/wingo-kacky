26618bb27e459e97d0251c6d4cd6c29863d1778a
LP B <xAt0mZ@users.noreply.github.com>
2023-08-13 17:33:01 +0200

    fix(app): unclickable checkbox

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -1,11 +1,28 @@
+import clsx from 'clsx';
+import { useState } from 'react';
+import { Switch } from '@headlessui/react';
+import { CheckIcon } from '@heroicons/react/24/outline';
+
 export function Checkbox({ label }: { label: string }) {
+  const [enabled, setEnabled] = useState(false);
+
   return (
-    <div className="flex flex-row gap-1">
-      <input
-        type="checkbox"
-        className="h-5 w-5 appearance-none rounded border border-theme-8 checked:text-theme-2"
-      />
-      <span className="text-base font-medium text-theme-2">{label}</span>
-    </div>
+    <Switch.Group>
+      <div className="flex items-center gap-1">
+        <Switch
+          checked={enabled}
+          onChange={setEnabled}
+          className="inline-flex h-5 w-5 rounded border border-theme-8 bg-theme-7 text-theme-2"
+        >
+          <CheckIcon
+            className={clsx(
+              'transition-opacity',
+              enabled ? 'opacity-100' : 'opacity-0'
+            )}
+          />
+        </Switch>
+        <Switch.Label className="cursor-pointer">{label}</Switch.Label>
+      </div>
+    </Switch.Group>
   );
 }

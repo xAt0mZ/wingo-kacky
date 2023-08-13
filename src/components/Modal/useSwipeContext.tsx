c68d1913c3a53d285f29a4e7c42d86f9a597780f
LP B <xAt0mZ@users.noreply.github.com>
2023-08-14 01:42:19 +0200

    refactor(app): extract protected swipe logic to SwipeProvider

---
+++ src/components/Modal/useSwipeContext.tsx
@@ -0,0 +1,104 @@
+import {
+  MutableRefObject,
+  PropsWithChildren,
+  createContext,
+  useContext,
+  useMemo,
+  useRef,
+  useState,
+} from 'react';
+import {
+  useSwipeable,
+  SwipeableProps,
+  SwipeableHandlers,
+} from 'react-swipeable';
+import { Position, useScrollPosition } from './useScrollPosition';
+
+type State = {
+  scrollableRef: MutableRefObject<null>;
+  scrollboxRef: MutableRefObject<null>;
+  swipeZoneHandlers: SwipeableHandlers;
+};
+
+const Context = createContext<State | null>(null);
+
+export function useSwipeContext(): State {
+  const ctx = useContext(Context);
+  if (!ctx) {
+    throw new Error('useSwipeContext must be used inside a SwipeProvider');
+  }
+  return ctx;
+}
+
+type Props = SwipeableProps;
+
+export function SwipeProvider({
+  children,
+  onSwipedDown,
+  onSwipedLeft,
+  onSwipedRight,
+  onSwipedUp,
+  onSwipeStart,
+  ...remainingCallbacks
+}: PropsWithChildren<Props>) {
+  const scrollableRef = useRef(null);
+  const scrollboxRef = useRef(null);
+
+  const [elemPosition, setElemPosition] = useState<Position>({ x: 0, y: 0 });
+  const [startPosition, setStartPosition] = useState<Position>({ x: 0, y: 0 });
+
+  const swippeableCallbacks: SwipeableProps = useMemo(
+    () => ({
+      onSwipedDown: (data) => {
+        if (startPosition.y === 0 && elemPosition.y === 0 && onSwipedDown) {
+          onSwipedDown(data);
+        }
+      },
+      onSwipedLeft: (data) => {
+        if (elemPosition.x === 0 && onSwipedLeft) {
+          onSwipedLeft(data);
+        }
+      },
+      onSwipedRight: (data) => {
+        if (elemPosition.x === 0 && onSwipedRight) {
+          onSwipedRight(data);
+        }
+      },
+      onSwipedUp: (data) => {
+        if (elemPosition.y === 0 && onSwipedUp) {
+          onSwipedUp(data);
+        }
+      },
+      onSwipeStart: (data) => {
+        setStartPosition(elemPosition);
+        onSwipeStart && onSwipeStart(data);
+      },
+      ...remainingCallbacks,
+    }),
+    [
+      elemPosition,
+      onSwipeStart,
+      onSwipedDown,
+      onSwipedLeft,
+      onSwipedRight,
+      onSwipedUp,
+      remainingCallbacks,
+      startPosition.y,
+    ]
+  );
+
+  const handlers = useSwipeable(swippeableCallbacks);
+
+  useScrollPosition({
+    effect: ({ currPos }) => setElemPosition(currPos),
+    element: scrollableRef,
+    boundingElement: scrollboxRef,
+  });
+
+  const state: State = useMemo(
+    () => ({ scrollableRef, scrollboxRef, swipeZoneHandlers: handlers }),
+    [handlers]
+  );
+
+  return <Context.Provider value={state}>{children}</Context.Provider>;
+}

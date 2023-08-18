8e3c0af7b161ee5077f6adb473afdd31b003751d
LP B <xAt0mZ@users.noreply.github.com>
2023-08-18 18:05:08 +0200

    feat(stats): header in stats view

--- src/StatsView/StatsView.tsx
+++ src/StatsView/StatsView.tsx
@@ -1,9 +1,14 @@
+import { Header } from '@/components/Header';
+
 import { WIPPanel } from '../components/WipPanel';
 
 export function StatsView() {
   return (
-    <div className="flex grow items-center justify-center">
-      <WIPPanel />
-    </div>
+    <>
+      <Header title="Statistiques" />
+      <div className="flex grow items-center justify-center">
+        <WIPPanel />
+      </div>
+    </>
   );
 }

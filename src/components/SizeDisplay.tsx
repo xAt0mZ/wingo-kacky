b9f8035551c1a8a70ded6d9fcd9c20ca7b97c9c5
LP B <xAt0mZ@users.noreply.github.com>
2023-08-18 16:42:06 +0200

    refactor(app): hide debug elements

--- src/components/SizeDisplay.tsx
+++ src/components/SizeDisplay.tsx
@@ -1,12 +1,13 @@
 export function SizeDisplay() {
-  return (
-    <div className="text-center">
-      <span className="sm:hidden">xs</span>
-      <span className="hidden sm:block md:hidden">sm</span>
-      <span className="hidden md:block lg:hidden ">md</span>
-      <span className="hidden lg:block xl:hidden ">lg</span>
-      <span className="hidden xl:block 2xl:hidden ">XL</span>
-      <span className="hidden 2xl:block ">2XL</span>
-    </div>
-  );
+  return null;
+  // return (
+  //   <div className="text-center">
+  //     <span className="sm:hidden">xs</span>
+  //     <span className="hidden sm:block md:hidden">sm</span>
+  //     <span className="hidden md:block lg:hidden ">md</span>
+  //     <span className="hidden lg:block xl:hidden ">lg</span>
+  //     <span className="hidden xl:block 2xl:hidden ">XL</span>
+  //     <span className="hidden 2xl:block ">2XL</span>
+  //   </div>
+  // );
 }

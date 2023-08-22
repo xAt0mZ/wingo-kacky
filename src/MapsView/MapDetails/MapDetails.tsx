4cc5993097d9e74ce07d5f27e2ca7016b8bf5293
LP B <xAt0mZ@users.noreply.github.com>
2023-08-22 14:05:11 +0200

    feat(details): tmp message for 239

--- src/MapsView/MapDetails/MapDetails.tsx
+++ src/MapsView/MapDetails/MapDetails.tsx
@@ -138,7 +138,10 @@ function Video() {
 
   return (
     <div className="flex flex-col items-stretch gap-4">
-      <span className="text-base font-semibold">Clip de démo</span>
+      <span className="text-base font-semibold">
+        Clip de {selectedMap.validated ? 'finish' : 'demo'}
+        {selectedMap.number === 239 ? " (oui, c'est le finish de Wingo)" : ''}
+      </span>
       <div className="flex flex-col items-start rounded-2xl bg-theme-7 p-3 shadow-md">
         <VideoPlayer url={selectedMap.video} />
       </div>

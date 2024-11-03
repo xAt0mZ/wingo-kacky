e5f9d24500626745462041414cfbe5f0a53ee80c
LP B <xAt0mZ@users.noreply.github.com>
2024-11-03 12:27:51 +0100

    feat(map-details): support imgur videos

--- src/MapsView/MapDetails/VideoPlayer.tsx
+++ src/MapsView/MapDetails/VideoPlayer.tsx
@@ -5,16 +5,29 @@ type VideoPlayerProps = {
   url?: string;
 };
 export function VideoPlayer({ url }: VideoPlayerProps) {
+  const imgur = isImgur(url);
   return (
     <div className="relative w-full overflow-hidden pt-[56.25%]">
-      {url && (
-        <iframe
-          className="absolute inset-0 h-full w-full rounded-lg"
-          title="Map clip"
-          src={transformUrl(url)}
-          allowFullScreen
-        />
-      )}
+      {url &&
+        (imgur ? (
+          <video
+            className="absolute inset-0 h-full w-full rounded-lg"
+            title="Map clip"
+            preload="auto"
+            autoPlay={false}
+            loop={false}
+            controls
+          >
+            <source src={`https://i.imgur.com/${imgur}.mp4`} />
+          </video>
+        ) : (
+          <iframe
+            className="absolute inset-0 h-full w-full rounded-lg"
+            title="Map clip"
+            src={transformUrl(url)}
+            allowFullScreen
+          />
+        ))}
       {!url && (
         <div className="absolute inset-0 flex h-full w-full items-center justify-center rounded-lg bg-theme-8">
           <VideoCameraSlashIcon className="h-1/4 text-theme-2" />
@@ -22,6 +35,30 @@ export function VideoPlayer({ url }: VideoPlayerProps) {
       )}
     </div>
   );
+}
+
+function isImgur(str?: string) {
+  if (!str) {
+    return false;
+  }
+
+  // imgur direct link
+  // IN https://i.imgur.com/123412341234.mp4
+  // OUT 123412341234
+  let matchArray = str.match(/(https:\/\/i.imgur.com\/)(.+?)\.(.+?)/);
+  if (matchArray) {
+    return matchArray[2];
+  }
+
+  // imgur full page
+  // IN https://imgur.com/123412341234
+  // OUT 123412341234
+  matchArray = str.match(/(https:\/\/imgur.com\/)(.+?)/);
+  if (matchArray) {
+    return matchArray[2];
+  }
+
+  return false;
 }
 
 function transformUrl(str: string) {
@@ -80,5 +117,6 @@ function transformUrl(str: string) {
   if (matchArray) {
     return `https://www.youtube-nocookie.com/embed/${matchArray[2]}`;
   }
-  return '';
+
+  return str;
 }
